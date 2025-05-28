867f7dccadd1584efbef8d77c47e10d296c66fa9
aso-sou0226 <2301210@s.asojuku.ac.jp>
2025-05-28 11:41:03 +0900

    ログインログイン

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ログイン</h1>
+    ユーザー名:<input type="text" name="name"><br>
+    パスワード:<input type="text" name="password"><br>
+    <form action="form.php">
+        <input type="submit" value="ログイン">
+    </form>
+</body>
+
+</html>
