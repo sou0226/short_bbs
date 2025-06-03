0261d4554b835dd824b37440b6bc1c67007c4b28
nomura-seiki <2301185@s.asojuku.ac.jp>
2025-06-03 10:20:39 +0900

    のむら

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    ユーザ名：<input type="text" name="text"><br><br>
+    パスワード：<input type="password" name="password"><br><br>
+    <input type="submit" value="ログイン"> 
+</body>
+</html>
